486d94e8b513c11b50d327940b9d6e4264bcd6c2
Hirata Koji <ture_doragonta@yahoo.co.jp>
2019-07-31 22:03:14 +0900

    navimeshの仮実装

---
+++ Assets/hirata/script/NaviMeshTestMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaviMeshTestMove : MonoBehaviour
+{
+    public Transform goal;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goal.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
